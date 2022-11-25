f7d0211ea83ce74c0b0174563f4930e870efc5e1
pratiksha <pratikshambhore95@gmail.com>
2022-11-25 23:04:29 +0530

    Review

--- src/com/MyFirstProCode/TestClass.java
+++ src/com/MyFirstProCode/TestClass.java
@@ -3,7 +3,9 @@ package com.MyFirstProCode;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("test data");
+		//System.out.println("test data");
+		
+		System.out.println("New data");
 
 	}
 
